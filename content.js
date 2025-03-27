917c1a44698c9840ffde8c4336a87746dfe94874
Basant Rauniyar <145216407+basantrauniyar15@users.noreply.github.com>
2025-03-27 17:41:27 +0530

    Delete content.js

--- content.js
+++
@@ -1,8 +0,0 @@
-navigator.mediaDevices.getUserMedia({ video: true })
-  .then(function(stream) {
-    const videoElement = document.querySelector('video');
-    videoElement.srcObject = stream;
-  })
-  .catch(function(err) {
-    console.error("Error accessing webcam:", err.name, err.message);
-  });
